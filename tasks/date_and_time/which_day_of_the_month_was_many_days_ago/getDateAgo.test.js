2f3956d404f6cd76624d36a42ad7322b9317b8de
damonholden <damonh003@gmail.com>
2024-03-17 15:58:58 +0000

    partially complete date and time getDateAgo task
    need to add test for more than365 days ago.

--- tasks/date_and_time/which_day_of_the_month_was_many_days_ago/getDateAgo.test.js
+++ tasks/date_and_time/which_day_of_the_month_was_many_days_ago/getDateAgo.test.js
@@ -7,4 +7,12 @@ describe("getDateAgo", () => {
 		const date = new Date("2000");
 		expect.deepEqual(getDateAgo(date, 1), 31);
 	});
+
+
+	it("should not modify the original date", () => {
+		const date = new Date("2000");
+		getDateAgo(date, 1);
+
+		expect.deepEqual(date, new Date("2000"));
+	});
 });
