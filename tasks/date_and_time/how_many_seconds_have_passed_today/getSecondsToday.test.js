fc5ade0475485b6ccc12ff60fe29e454b3902215
damonholden <damonh003@gmail.com>
2024-03-21 23:50:26 +0000

    use the context parameter in nodes test callback function to mock the date

--- tasks/date_and_time/how_many_seconds_have_passed_today/getSecondsToday.test.js
+++ tasks/date_and_time/how_many_seconds_have_passed_today/getSecondsToday.test.js
@@ -2,6 +2,10 @@
 import { describe, it } from "node:test";
 import expect from "node:assert/strict";
 import { getSecondsToday } from "./getSecondsToday.js";
+import { isContext } from "node:vm";
 
 describe("getSecondsToday", () => {
+	it("should return the correct amount of seconds elapsed from the beginning of the day", (context) => {
+		context.mock.timers.enable({ apis: ['Date'] });
+	});
 });
