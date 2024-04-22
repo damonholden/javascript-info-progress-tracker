9621572ea9803079c7f4a682a8a6c7ad00cf1beb
damonh003 <damonh003@gmail.com>
2024-04-22 09:15:40 +0000

    implement and test full date requirement of formatDate

--- tasks/date_and_time/format_the_relative_date/formatDate.js
+++ tasks/date_and_time/format_the_relative_date/formatDate.js
@@ -16,4 +16,27 @@ export function formatDate(date) {
         const differenceAsMinutes = timeDifference / 1000 / 60;
         return `${differenceAsMinutes} min. ago`;
     }
+
+    let days = String(date.getDate());
+    if (days.length === 1) {
+        days = `0${days}`;
+    }
+
+    let month = String(date.getMonth() + 1);
+    if (month.length === 1) {
+        month = `0${month}`;
+    }
+
+    const year = String(date.getFullYear());
+    const lastTwoDigitsOfYear = year.substring(year.length - 2);
+
+    let hours = String(date.getHours());
+    if (hours.length === 1) {
+        hours = `0${hours}`;
+    }
+    let minutes = String(date.getMinutes());
+    if (minutes.length === 1) {
+        minutes = `0${minutes}`;
+    }
+    return `${days}.${month}.${lastTwoDigitsOfYear} ${hours}:${minutes}`;
 }
--- tasks/date_and_time/format_the_relative_date/formatDate.test.js
+++ tasks/date_and_time/format_the_relative_date/formatDate.test.js
@@ -15,9 +15,16 @@ describe("formatDate", () => {
         assert.strictEqual(formatDate(date), "50 sec. ago");
     });
 
-    test("returns 'n min. ago' if the passed in date is more than a second but less than a minute ago", (context) => {
+    test("returns 'n min. ago' if the passed in date is more than a minute but less than an hour ago", (context) => {
         context.mock.timers.enable({ apis: ["Date"], now: january1st2000 });
         const date = new Date(january1st2000 - 3_000_000);
         assert.strictEqual(formatDate(date), "50 min. ago");
+    });
+
+    test('returns the full date in the format "DD.MM.YY HH:mm". That is: "day.month.year hours:minutes", all in 2-digit format', (context) => {
+        context.mock.timers.enable({ apis: ["Date"], now: january1st2000 });
+        const twoHoursInMilliseconds = 7_200_000;
+        const date = new Date(january1st2000 - twoHoursInMilliseconds);
+        assert.strictEqual(formatDate(date), "01.01.00 08:00");
     });
 });
