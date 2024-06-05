269b93cc84437876ceab5349a31c9d954a0b7742
damonh003 <damonh003@gmail.com>
2024-06-05 14:57:07 +0000

    add sumToArithmeticProgression code and tests

---
+++ tasks/recursion_and_stack/sum_all_numbers_till_the_given_one/sumToArithmeticProgression.js
@@ -0,0 +1,3 @@
+export function sumToArithmeticProgression(n) {
+    return (n * (n + 1)) / 2;
+}
---
+++ tasks/recursion_and_stack/sum_all_numbers_till_the_given_one/sumToArithmeticProgression.test.js
@@ -0,0 +1,25 @@
+import { describe, test } from "node:test";
+import assert from "node:assert/strict";
+import { sumToArithmeticProgression } from "./sumToArithmeticProgression.js";
+
+describe("sumToArithmeticProgression", () => {
+    test("returns 1 for 1", () => {
+        assert.strictEqual(sumToArithmeticProgression(1), 1);
+    });
+
+    test("returns 3 for 2", () => {
+        assert.strictEqual(sumToArithmeticProgression(2), 3);
+    });
+
+    test("returns 6 for 3", () => {
+        assert.strictEqual(sumToArithmeticProgression(3), 6);
+    });
+
+    test("returns 10 for 4", () => {
+        assert.strictEqual(sumToArithmeticProgression(4), 10);
+    });
+
+    test("returns 15 for 5", () => {
+        assert.strictEqual(sumToArithmeticProgression(5), 15);
+    });
+});
